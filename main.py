e864427f4bc7ee77ddecda10c7893b75ccfdf3ee
Sahil Patil <sahilpatil@Sahils-MacBook-Air.local>
2025-07-19 16:27:58 +0530

    Initial commit

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def static_files(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
